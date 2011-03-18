e67c597697eef53eba9c07ada28a2db8d253f4f0
jessor <daniel@enthusiasm.cc>
2011-03-18 02:18:06 +0100

    Revert "switched to syntax highlighting with albino (pygments), let's see if it works on heroku" -- well, it didn't
    This reverts commit 580c8abc3ea7aca6a4e5f939a929a999ffbc620d.

--- toopaste.rb
+++ toopaste.rb
@@ -5,11 +5,13 @@ require 'dm-core'
 require 'dm-validations'
 require 'dm-timestamps'
 require 'dm-migrations'
+require 'rack/codehighlighter'
 require 'haml'
 require 'sass'
+require 'uv'
 require 'rack-flash'
-require 'albino'
 
+use Rack::Codehighlighter, :ultraviolet, :lines => true, :markdown => false, :element => "pre"
 use Rack::Flash
 enable :sessions
 set :haml, :format => :html5
@@ -68,9 +70,9 @@ get '/:id' do
   @snippet = Snippet.get(params[:id])
   if @snippet
     if @snippet.language
-      @snippet.body = Albino.new(@snippet.body, :ruby).colorize
+      @snippet.body = ":::#{h @snippet.language.downcase}\n#{@snippet.body}"
     else
-      @snippet.body = Albino.new(@snippet.body).colorize
+      @snippet.body = ":::blog_text\n#{@snippet.body}\n"
     end
     haml :show
   else
